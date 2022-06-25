4a65922a820d9755bb66dc19eb61047a57569210
Mike <bottone@gmail.com>
2022-06-24 22:21:13 -0400

    further styling

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ const apiCall = async () => {
   if (response.data.hello === '') {
     helloContainer.innerHTML = `<h1>Oops, I don't know how to say hello in ${countryName}\'s language</h1>`
   } else {
-    helloContainer.innerHTML = `<h1>You can say "hello" in ${countryName} like this:<br> <span id="hola">${response.data.hello}</span></h1>`
+    helloContainer.innerHTML = `<h1>You can say <em>"hello"</em> in ${countryName} like this:<br> <span id="hola">${response.data.hello}</span></h1>`
   }
   // helloContainer.innerHTML += coordinates
   mapContainer.innerHTML = `<iframe width="500" height="400" frameborder="0" src="https://www.bing.com/maps/embed?h=500&w=500&cp=${coordinatesA}~${coordinatesB}&lvl=6&typ=s&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
