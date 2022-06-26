ded8429a0f6cd376e9b4a824fb789bff4282ca93
Mike <bottone@gmail.com>
2022-06-26 10:25:14 -0400

    second page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const helloContainer = document.querySelector('#hello-container')
 const locationContainer = document.querySelector('#location')
 const mapContainer = document.querySelector('#map-container')
 const apiKey =
-  'jsonp=GeocodeCallback&key=AiwZaKSOpDkgpIQGX2rMFIsMkvcd_Nv98haULE_IvmMaDv4H5ckuSlGYt3hLoYY-'
+  'AiwZaKSOpDkgpIQGX2rMFIsMkvcd_Nv98haULE_IvmMaDv4H5ckuSlGYt3hLoYY-'
 const countryArray = [
   '',
   'Afghanistan',
@@ -237,30 +237,29 @@ const countryArray = [
 
 const apiCall = async () => {
   // window.open('_blank')
-  let hello = button.value
+  let countryCode = button.value
 
   let pick = button.selectedIndex
   let countryName = countryArray[pick]
 
   let response = await axios.get(
-    `https://fourtonfish.com/hellosalut/?cc=${hello}`
+    `https://fourtonfish.com/hellosalut/?cc=${countryCode}`
   )
   let mapRepsonse = await axios.get(
-    `https://dev.virtualearth.net/REST/v1/Locations?query='${countryName}'${apiKey}`
+    `https://dev.virtualearth.net/REST/v1/Locations?query='${countryName}'jsonp=GeocodeCallback&key=${apiKey}`
   )
   let coordinatesA =
     mapRepsonse.data.resourceSets[0].resources[0].point.coordinates[0]
   let coordinatesB =
     mapRepsonse.data.resourceSets[0].resources[0].point.coordinates[1]
-  if (response.data.hello === '') {
+  if (response.data.countryCode === '') {
     helloContainer.style.display = 'none'
   } else {
     helloContainer.innerHTML = `<h1>You can say <em>"hello"</em> while your're in ${countryName} like this:<br> <span id="hola">${response.data.hello}</span></h1>`
   }
   GetMap = () => {
     let map = new Microsoft.Maps.Map(mapContainer, {
-      credentials:
-        'AiwZaKSOpDkgpIQGX2rMFIsMkvcd_Nv98haULE_IvmMaDv4H5ckuSlGYt3hLoYY-',
+      credentials: apiKey,
       center: new Microsoft.Maps.Location(coordinatesA, coordinatesB)
     })
     let center = map.getCenter()
@@ -269,7 +268,7 @@ const apiCall = async () => {
       color: 'blue'
     })
     map.setView({
-      zoom: 3
+      zoom: 4
     })
 
     map.entities.push(pin)
@@ -280,3 +279,4 @@ const apiCall = async () => {
 }
 
 button.addEventListener('change', apiCall)
+selectCountry.addEventListener()
